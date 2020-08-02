7e1f7d5ffd640284c2c81883ad6f85b451e91323
Keenan Wilkinson <keenanwilkinson@outlook.com>
2020-08-01 20:32:37 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+read_README = open('README.txt', mode='r')
+README = "".join(read_README.readlines())
+read_README.close()
+
 setup(
     name="KnitCryption",
-    version="2.0.7",
+    version="2.0.8",
     packages=find_packages(),
     scripts=[],
     install_requires=["docutils>=0.3"],
@@ -12,6 +16,7 @@ setup(
     author="Keenan W. Wilkinson",
     author_email="keenanwilkinson@outlook.com",
     description="Encrytion package to assist in hiding data",
+    long_description=README,
     license="GPL-3.0 license",
     url="https://github.com/WilkinsonK/KnitCryption"
 )
